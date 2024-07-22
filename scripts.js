d0a311cf32fe7856ac3d48b07173ff3aba7c5a40
林琮原 <161666401+Yuan1285270@users.noreply.github.com>
2024-07-22 18:17:46 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document loaded');
+    let heading = document.querySelector('h1');
+    heading.textContent = '歡迎來到我的網站';
+});
